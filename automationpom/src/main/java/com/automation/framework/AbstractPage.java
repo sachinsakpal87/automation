a326fb360fbb7adc2d53efcc816144b0d6728150
sachinsakpal <sachin.sakpal@appdirect.com>
2018-09-22 22:30:55 +0530

    Updated Abstract.java class handled few preconditions and sonar checkes

--- automationpom/src/main/java/com/automation/framework/AbstractPage.java
+++ automationpom/src/main/java/com/automation/framework/AbstractPage.java
@@ -3,6 +3,7 @@ package com.automation.framework;
 import com.automation.reports.ExtentTestManager;
 import com.google.common.collect.ImmutableSet;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.internal.Locatable;
@@ -22,6 +23,7 @@ import java.util.function.Function;
 
 import static com.automation.reports.ExtentTestManager.logWarning;
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
@@ -43,6 +45,7 @@ public abstract class AbstractPage {
     }
 
     private void safeClick(WebElement webElement) {
+        checkNotNull(webElement, "WebElement can not be null");
         wait("Wail till element displayed ", webElement, 5);
         action("Click", webElement, arg -> arg.click(), this);
     }
@@ -85,38 +88,41 @@ public abstract class AbstractPage {
         return action(element, action, page);
     }
 
-    protected <T extends AbstractPage> T action(WebElement element, Consumer<WebElement> action, T page) {
-        checkArgument(nonNull(element), "WebElement can not be null");
-        action(element, action);
+    protected <T extends AbstractPage> T action(WebElement webElement, Consumer<WebElement> action, T page) {
+        checkNotNull(webElement, "WebElement can not be null");
+        action(webElement, action);
         return page;
     }
 
-    protected AbstractPage action(String logMessage, WebElement element, Consumer<WebElement> action) {
+    protected AbstractPage action(String logMessage, WebElement webElement, Consumer<WebElement> action) {
         checkArgument(isNotBlank(logMessage), LOG_MESSAGE_CAN_NOT_BE_NULL_OR_EMPTY);
+        checkNotNull(webElement, "WebElement can not be null");
         ExtentTestManager.logInfo(logMessage);
-        return action(element, action);
+        return action(webElement, action);
     }
 
-    protected AbstractPage action(WebElement element, Consumer<WebElement> action) {
-        checkArgument(nonNull(element), "WebElement can not be null");
-        action.accept(element);
+    protected AbstractPage action(WebElement webElement, Consumer<WebElement> action) {
+        checkNotNull(webElement, "WebElement can not be null");
+        action.accept(webElement);
         return this;
     }
 
     protected <T extends AbstractPage> T action(String logMessage, Select element, Consumer<Select> action, T page) {
-        action(logMessage,element,action);
+        checkArgument(isNotBlank(logMessage), LOG_MESSAGE_CAN_NOT_BE_NULL_OR_EMPTY);
+        checkNotNull(element, "Select can not be null");
+        action(logMessage, element, action);
         return page;
     }
 
     protected AbstractPage action(String logMessage, Select element, Consumer<Select> action) {
         checkArgument(isNotBlank(logMessage), LOG_MESSAGE_CAN_NOT_BE_NULL_OR_EMPTY);
-        checkArgument(nonNull(element), "Select can not be null");
+        checkNotNull(element, "Select can not be null");
         ExtentTestManager.logInfo(logMessage);
         return action(element, action);
     }
 
     protected AbstractPage action(Select element, Consumer<Select> action) {
-        checkArgument(nonNull(element), "WebElement can not be null");
+        checkNotNull(element, "Select can not be null");
         action.accept(element);
         return this;
     }
@@ -125,12 +131,12 @@ public abstract class AbstractPage {
         return wait(logMessage, ExpectedConditions.visibilityOf(element), 5);
     }
 
-    protected AbstractPage wait(String logMessage, WebElement element, Function<WebElement, Boolean> condition, int timeout) {
+    protected AbstractPage wait(String logMessage, WebElement webElement, Function<WebElement, Boolean> condition, int timeout) {
         checkArgument(isNotBlank(logMessage), LOG_MESSAGE_CAN_NOT_BE_NULL_OR_EMPTY);
-        checkArgument(nonNull(element), "WebElement can not be null");
+        checkNotNull(webElement, "WebElement can not be null");
         ExtentTestManager.logInfo(logMessage);
 
-        new FluentWait<>(element)
+        new FluentWait<>(webElement)
                 .withTimeout(Duration.ofSeconds(timeout))
                 .pollingEvery(Duration.ofSeconds(5))
                 .ignoreAll(IGNORE_EXCEPTIONS_WHILE_WAITING_SET)
@@ -149,6 +155,7 @@ public abstract class AbstractPage {
     }
 
     protected AbstractPage moveToElement(WebElement webElement) {
+        checkNotNull(webElement, "WebElement can not be null");
         return action("Moving to Element", webElement, we -> new Actions(getDriver()).moveToElement(we).perform(), this);
     }
 
@@ -165,10 +172,12 @@ public abstract class AbstractPage {
     }
 
     protected AbstractPage switchToWindow(String windowName) {
+        checkArgument(StringUtils.isNotBlank(windowName),"Window name can not be null or blank");
         return switchToWindow(windowName, 5);
     }
 
     protected AbstractPage switchToWindow(String windowName, int timeout) {
+        checkArgument(StringUtils.isNotBlank(windowName),"Window name can not be null or blank");
         new FluentWait<>(getDriver())
                 .withTimeout(Duration.ofSeconds(timeout))
                 .pollingEvery(Duration.ofSeconds(5))
@@ -178,10 +187,12 @@ public abstract class AbstractPage {
     }
 
     protected AbstractPage switchToiFrame(String frameName) {
+        checkArgument(StringUtils.isNotBlank(frameName),"Frame name can not be null or blank");
         return switchToiFrame(frameName, 5);
     }
 
     protected AbstractPage switchToiFrame(String frameName, int timeout) {
+        checkArgument(StringUtils.isNotBlank(frameName),"Frame name can not be null or blank");
         new FluentWait<>(getDriver())
                 .withTimeout(Duration.ofSeconds(timeout))
                 .pollingEvery(Duration.ofSeconds(5))
@@ -208,6 +219,8 @@ public abstract class AbstractPage {
     }
 
     protected void dragAndDrop(WebElement source, WebElement destination) {
+        checkNotNull(source, "Source WebElement can not be null");
+        checkNotNull(destination, "Destination WebElement can not be null");
         action("Drag and Drop", source, we -> new Actions(getDriver()).dragAndDrop(source, destination).perform());
     }
 
@@ -216,6 +229,8 @@ public abstract class AbstractPage {
      */
     @Deprecated
     protected void dragAndDrop(WebElement source, WebElement destination, boolean old) {
+        checkNotNull(source, "Source WebElement can not be null");
+        checkNotNull(destination, "Destination WebElement can not be null");
         action("click and hold", source, we -> new Actions(getDriver()).clickAndHold(source));
         moveToElement(destination);
         action("release", source, we -> new Actions(getDriver()).release());
@@ -226,6 +241,7 @@ public abstract class AbstractPage {
      */
     @Deprecated
     protected void dragAndDropBy(WebElement source, int xOffset, int yOffset) {
+        checkNotNull(source, "Source WebElement can not be null");
         new Actions(getDriver()).dragAndDropBy(source, xOffset, yOffset).perform();
     }
 
@@ -233,43 +249,72 @@ public abstract class AbstractPage {
      * @deprecated (when, can directly use action is calling class, keeping for reference...)
      */
     @Deprecated
-    protected void rightClick(WebElement element) {
-        action("Right click", element, pe -> new Actions(getDriver()).moveToElement(pe).contextClick(pe).perform());
+    protected void rightClick(WebElement webElement) {
+        checkNotNull(webElement, "WebElement can not be null");
+        action("Right click", webElement, pe -> new Actions(getDriver()).moveToElement(pe).contextClick(pe).perform());
     }
 
-    protected void scrollIntoView(WebElement element){
-        action(element,pe->((JavascriptExecutor)getDriver()).executeScript("argument[0].scrollIntoView(true);",pe));
+    protected void scrollIntoView(WebElement webElement) {
+        checkNotNull(webElement, "WebElement can not be null");
+        action(webElement, pe -> ((JavascriptExecutor) getDriver()).executeScript("argument[0].scrollIntoView(true);", pe));
     }
 
-    protected void scrollDown(){
-        ((JavascriptExecutor)getDriver()).executeScript("window.scrollBy(0,250)","");
+    protected void scrollDown() {
+        ((JavascriptExecutor) getDriver()).executeScript("window.scrollBy(0,250)", "");
     }
 
-    protected void scrollUp(){
-        ((JavascriptExecutor)getDriver()).executeScript("window.scrollBy(250,0)","");
+    protected void scrollUp() {
+        ((JavascriptExecutor) getDriver()).executeScript("window.scrollBy(250,0)", "");
     }
 
-    protected void scrollByCordinates(WebElement element){
-        ((Locatable)element).getCoordinates().inViewPort();
+    protected void scrollByCordinates(WebElement webElement) {
+        checkNotNull(webElement, "WebElement can not be null");
+        ((Locatable) webElement).getCoordinates().inViewPort();
     }
 
-    protected  boolean brokenImage(WebElement element){
+    protected boolean brokenImage(WebElement webElement) {
+        checkNotNull(webElement, "WebElement can not be null");
         EventFiringWebDriver eventFiringWebDriver = new EventFiringWebDriver(getDriver());
         String script = "return (typeof arguments[0].naturalWidth !='undefined' && arguments[0].naturalWidth>0)";
-        return (boolean)eventFiringWebDriver.executeScript(script,element);
+        return (boolean) eventFiringWebDriver.executeScript(script, webElement);
     }
 
-    protected String getPageSource(){
+    protected String getPageSource() {
         return getDriver().getPageSource();
     }
 
+    protected void enterText(String text, WebElement webElement) {
+        checkArgument(StringUtils.isNotBlank(text), "text can not be empty or blank");
+        checkNotNull(webElement, "WebElement can not be null");
+        action(webElement, WebElement::clear);
+        action(String.format("clear and enter text '%s'", text), webElement, we -> we.sendKeys(text));
+    }
+
+    protected void jsEnterText(String text, WebElement webElement) {
+        checkArgument(StringUtils.isNotBlank(text), "text can not be empty or blank");
+        checkNotNull(webElement, "WebElement can not be null");
+        action("click", webElement, we -> ((JavascriptExecutor) this.getDriver()).executeScript("arguments[0].value='" + text + "';", we));
+    }
+
+    protected void jsClick(WebElement webElement) {
+        checkArgument(nonNull(webElement), "WebElement can not be null");
+        action("click", webElement, we -> ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click()", we));
+    }
+
     protected void click(WebElement webElement) {
+        checkNotNull(webElement, "WebElement can not be null");
         try {
             safeClick(webElement);
         } catch (TimeoutException te) {
             logWarning("Could not click element, attempt to move to element and click");
             moveToElement(webElement);
-            safeClick(webElement);
+            try {
+                safeClick(webElement);
+            } catch (TimeoutException ex) {
+                logWarning("Could not click element, attempt to scroll to view to element and click");
+                scrollIntoView(webElement);
+                safeClick(webElement);
+            }
         }
     }
 }
