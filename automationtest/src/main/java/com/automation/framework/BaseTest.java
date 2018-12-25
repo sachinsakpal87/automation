c9421e4a378de13a66a639f52bdd696e529c8339
quail0301 <quail03011986@gmail.com>
2018-12-25 16:46:36 +0530

    Committed base test changes

--- automationtest/src/main/java/com/automation/framework/BaseTest.java
+++ automationtest/src/main/java/com/automation/framework/BaseTest.java
@@ -1,6 +1,9 @@
 package com.automation.framework;
 
 import com.automation.CustomContext;
+import com.automation.core.ApiWrapper;
+import com.automation.environment.EnvironmentProperties;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -9,6 +12,9 @@ import org.testng.annotations.AfterMethod;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import static com.automation.reports.ExtentTestManager.*;
 
@@ -16,9 +22,12 @@ public class BaseTest extends AbstractBaseTest {
 
     private final CustomContext customContext;
     private final ThreadLocal<WebUser> web = new ThreadLocal<>();
+    private EnvironmentProperties environmentProperties;
+    private ApiWrapper apiWrapper;
 
     public BaseTest(CustomContext customContext) {
         this.customContext = customContext;
+        environmentProperties = new EnvironmentProperties();
     }
 
     private CustomContext getCustomContext() {
@@ -50,5 +59,27 @@ public class BaseTest extends AbstractBaseTest {
             }
         }
         web.get().getDriver().close();
+    }
+
+    public ApiWrapper apiRobot() {
+        if (apiWrapper == null) {
+            apiWrapper = new ApiWrapper(environmentProperties);
+            apiWrapper.setBaseUrl(buildBaseUrl());
+        }
+        return apiWrapper;
+    }
+
+    private String buildBaseUrl() {
+        try {
+            URIBuilder builder = new URIBuilder();
+            builder.setScheme(environmentProperties.getEnvironmentProperty("protocol", false));
+            builder.setHost(environmentProperties.getEnvironmentProperty("host", false));
+            URL url = builder.build().toURL();
+            return url.toString();
+        } catch (URISyntaxException e) {
+            return "";
+        } catch (MalformedURLException e) {
+            return "";
+        }
     }
 }
